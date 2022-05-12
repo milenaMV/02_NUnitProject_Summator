8864a0051421ec715df3fe9bfe69c45dd318e0ac
milenaMV <105450687+milenaMV@users.noreply.github.com>
2022-05-12 21:56:10 +0300

    Initial Commit

---
+++ Summator/Summator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static int Average(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+    }
+}
